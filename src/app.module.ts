c79ad4fb178bdfa014106fe95bccf397efbf84a4
leoSantos@Fedora <leosantosmelo07@gmail.com>
2026-02-22 11:11:51 -0300

    fix: ajuste dto bird e app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BirdsModule } from './modules/birds/birds.module';
+import { PrismaService } from './database';
 
 @Module({
   imports: [BirdsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [PrismaService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/modules/birds/dto/create-bird.dto.ts
+++ src/modules/birds/dto/create-bird.dto.ts
@@ -1,1 +1,6 @@
-export class CreateBirdDto {}
+export class CreateBirdDto {
+    name: string
+    cientific_name: string
+    description: string
+    habitats: Array<string>
+}
