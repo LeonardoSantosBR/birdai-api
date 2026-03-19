2d997ebb91e4bbf7162490676b0d0bc991301a68
Leonardo Santos <leosantosmelo07@gmail.com>
2026-03-19 20:52:42 -0300

    style: eslint

--- src/modules/habitats/habitats.service.ts
+++ src/modules/habitats/habitats.service.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Injectable } from '@nestjs/common';
 import { HabitatsRepository } from './habitats.repository';
 import { habitats, Prisma } from '@prisma/client';
