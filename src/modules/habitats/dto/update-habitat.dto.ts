5b643d681423beeaac2ce68090af767de78455a0
Leonardo Santos <leosantosmelo07@gmail.com>
2026-03-22 11:12:11 -0300

    feat: delete onCascade

--- src/modules/habitats/dto/update-habitat.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateHabitatDto } from './create-habitat.dto';
-
-export class UpdateHabitatDto extends PartialType(CreateHabitatDto) {}
